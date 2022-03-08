949d75fc15e783adc519e3dcb7030a733023ce11
Basil <basel_marwan@icloud.com>
2022-03-08 10:29:15 +0300

    daily data

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,4 +1,20 @@
+import { useState, useEffect } from 'react';
+import { getDailyData } from '../../api';
+
 const Chart = () => {
+  const [dailyData, setDailyData] = useState({});
+
+  useEffect(() => {
+    async function fetchMyAPI() {
+      const fetchData = await getDailyData();
+
+      setDailyData(fetchData);
+    }
+
+    fetchMyAPI();
+
+    console.log(dailyData);
+  });
   return <h2>Chart</h2>;
 };
 
